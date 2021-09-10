1bfecf8d203e673048e8933c86b1f0698d9b3f44
Andrey Vakulenko <57860992+pizzlo@users.noreply.github.com>
2021-09-10 04:16:53 +0300

    Update filecsv.py

--- filecsv.py
+++ filecsv.py
@@ -1,8 +1,5 @@
 import csv
 import pandas as pd
 acme=pd.read_csv("acme_worksheet.csv")
-df=pd.pivot_table(acme,
-               index=["Employee_Name"],
-               values=["Work_Hours"],
-               columns=["Date"])
+df=pd.pivot_table(acme,index=["Employee_Name"],values=["Work_Hours"],columns=["Date"])
 df.to_csv('out.csv', encoding='utf-8')
